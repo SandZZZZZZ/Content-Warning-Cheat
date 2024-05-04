6fc09e49a2b5e4a99f48f6be9164dcc66cf8381e
SandZZZZZZ <134781936+SandZZZZZZ@users.noreply.github.com>
2024-05-05 04:07:10 +0800

    Update Players.cs

--- Players.cs
+++ Players.cs
@@ -260,8 +260,8 @@ namespace ContentWarningCheat
         public static List<string> Name = new List<string>
         {
             //在这里自定义你的滚动昵称
-            "Powered By xiaodo1337.",
-            "此作弊模组免费开源，如果你是购买获得，恭喜你被圈钱了！"
+            "洗洗更健康",
+            "原神 启动！"
         };
         public static int CurrentNamePos = 0;
         public static float ColorHUE = 0.001f;
